1c3532dbcd19b12569401bd76e528e3eb8a51114
Ulrike <ulrikebloch@icloud.com>
2024-09-13 15:48:37 +0200

    fix bug

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { login, signup } from './actions'
 export default function LoginPage() {
   return (
     <form>
-      <label htmlFor="email">Your Email:</label>
+      <label htmlFor="email">Email:</label>
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
